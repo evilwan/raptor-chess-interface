4137aa99fc5f3fc57d12f35cba52503f6a2d7ca3
carsonday <carsonday>
2009-08-30 22:51:28 +0000

    Initial prototype check in.

--- src/raptor/service/SoundService.java
+++ src/raptor/service/SoundService.java
@@ -5,10 +5,12 @@ public class SoundService {
 	public static final int GAME_OBS_START = 0;
 	public static final int GAME_OBS_END = 1;
 	public static final int GAME_OBS_MOVE = 2;
-	public static final int GAME_PLAY_START = 3;
-	public static final int GAME_PLAY_END = 4;
-	public static final int GAME_PLAY_MOVE = 5;
-	public static final int TELL = 6;
+	public static final int GAME_OBS_ILLEGAL = 3;
+	public static final int GAME_PLAY_START = 4;
+	public static final int GAME_PLAY_END = 5;
+	public static final int GAME_PLAY_MOVE = 6;
+	public static final int GAME_PLAY_ILLEGAL = 7;
+	public static final int TELL = 8;
 
 	private static final SoundService instance = new SoundService();
 
