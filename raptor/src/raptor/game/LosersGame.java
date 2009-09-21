aaf6bf4c42fe09fc98521cbaae24900bc003f41c
john.nahlen@gmail.com <john.nahlen@gmail.com>
2009-09-21 22:58:10 +0000

    Additions to game types code

--- raptor/src/raptor/game/LosersGame.java
+++ raptor/src/raptor/game/LosersGame.java
@@ -1,5 +1,18 @@
 package raptor.game;
 
 public class LosersGame extends Game {
-
+	
+	public LosersGame() {
+		setType(Game.LOSERS);
+	}
+	
+	@Override
+	public PriorityMoveList getLegalMoves() {
+		return null;
+	}
+	
+	@Override
+	public boolean isLegalPosition() {
+		return false;
+	}
 }
