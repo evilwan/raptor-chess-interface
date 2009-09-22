727269d14d247161d972af7d00e9a6c5fdcb6bd9
john.nahlen@gmail.com <john.nahlen@gmail.com>
2009-09-22 02:31:14 +0000

    Additions to game types code

--- raptor/src/raptor/game/SuicideGame.java
+++ raptor/src/raptor/game/SuicideGame.java
@@ -1,18 +1,58 @@
 package raptor.game;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class SuicideGame extends Game {
+	private static final Log LOG = LogFactory.getLog(SuicideGame.class);
 	
 	public SuicideGame() {
 		setType(Game.SUICIDE);
 	}
 	
-	@Override
-	public PriorityMoveList getLegalMoves() {
-		return null;
+	public boolean isLegalPosition() {
+		return true;
 	}
 	
-	@Override
-	public boolean isLegalPosition() {
-		return false;
+	public PriorityMoveList getLegalMoves() {
+		PriorityMoveList result = getPseudoLegalMoves();
+
+		LOG.debug("Possible Moves = " + java.util.Arrays.toString(result.asArray()));
+		
+		boolean containsCaptures = false;
+		for (int i = 0; i < result.getHighPrioritySize(); i++) {
+			Move move = result.getHighPriority(i);
+			forceMove(move);
+
+			if (!isLegalPosition()) {
+				result.removeHighPriority(i);
+				i--;
+			}
+			else {
+			if (move.isCapture()) { containsCaptures = true; }
+			}
+			
+			rollback();
+		}
+		
+		LOG.debug("containsCapture = " + containsCaptures);
+		
+		for (int i = 0; i < result.getLowPrioritySize(); i++) {
+			if (containsCaptures) { result.removeLowPriority(i); i--; continue; } // remove all, since there are no non-capture legal moves if can capture
+			
+			Move move = result.getLowPriority(i);
+			forceMove(move);
+			
+			if (!isLegalPosition()/* || move.isCastling()*/) { // castling not allowed in suicide
+				result.removeLowPriority(i);
+				i--;
+			}
+
+			rollback();
+		}
+		
+		LOG.debug("Possible Moves = " + java.util.Arrays.toString(result.asArray()));
+		
+		return result;
 	}
 }
