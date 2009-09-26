3d4e7a33c97daa3d6bf98ea533ff1198d2b24873
john.nahlen@gmail.com <john.nahlen@gmail.com>
2009-09-26 05:04:23 +0000

    Renamed UnpartneredBugger to Bugger Moved static class to its own class Added Javadocs and more methods

--- raptor/src/raptor/connector/fics/chat/john/BugWhoUParser.java
+++ raptor/src/raptor/connector/fics/chat/john/BugWhoUParser.java
@@ -8,48 +8,16 @@ import org.apache.log4j.Logger;
 public class BugWhoUParser {
 	private static final Logger LOGGER = Logger.getLogger(BugWhoUParser.class);
 	
-	public static class UnpartneredBugger {
-		private String rating;
-		private char status;
-		private String username;
-		
-		public void setRating(String rating) {
-			this.rating = rating;
-		}
-		public String getRating() {
-			return rating;
-		}
-		
-		public void setStatus(char status) {
-			this.status = status;
-		}
-		public char getStatus() {
-			return status;
-		}
-		
-		public void setUsername(String username) {
-			this.username = username;
-		}
-		public String getUsername() {
-			return username;
-		}
-		
-		public String toString() {
-			return getUsername();
-		}
-		
-	}
-	
 	public BugWhoUParser(String text) {
 		text = text.replaceAll("[0-9]+ players displayed \\(of [0-9+]\\). \\(\\*\\) indicates system administrator.","");
 	}
 	
-	public UnpartneredBugger[] parse(String text,boolean showUnrateds,boolean showGuests) {
+	public Bugger[] parse(String text,boolean showUnrateds,boolean showGuests) {
 		StringTokenizer tokens = new StringTokenizer(text,"^~:#.& ",true);
-		ArrayList<UnpartneredBugger> arr = new ArrayList<UnpartneredBugger>();
+		ArrayList<Bugger> arr = new ArrayList<Bugger>();
 		
 		while(tokens.hasMoreTokens()) {
-			UnpartneredBugger p = new UnpartneredBugger();
+			Bugger p = new Bugger();
 			String rating = tokens.nextToken();
 			if (rating.equals("----") && !showUnrateds) continue;
 			if (rating.equals("++++") && !showGuests) continue;
@@ -63,7 +31,7 @@ public class BugWhoUParser {
 			
 			arr.add(p);
 		}
-		UnpartneredBugger[] out = arr.toArray(new UnpartneredBugger[arr.size()]);
+		Bugger[] out = arr.toArray(new Bugger[arr.size()]);
 		LOGGER.info("usernames = " + java.util.Arrays.toString(out));
 		return out;
 	}
