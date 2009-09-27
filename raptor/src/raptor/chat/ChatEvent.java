25e3de9ef485e50d76701d584d6bf1a37d80891d
john.nahlen@gmail.com <john.nahlen@gmail.com>
2009-09-27 05:57:27 +0000

    Added Javadocs

--- raptor/src/raptor/chat/ChatEvent.java
+++ raptor/src/raptor/chat/ChatEvent.java
@@ -32,18 +32,31 @@ public class ChatEvent {
 		return gameId;
 	}
 
+	/**
+	 * @return Entire message involved in this ChatEvent.
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * @return Username of the person involved in this ChatEvent.
+	 */
 	public String getSource() {
 		return source;
 	}
 
+	/**
+	 * @return What time this ChatEvent occurred, in milliseconds.
+	 */
 	public long getTime() {
 		return time;
 	}
 
+	/**
+	 * @return The type of ChatEvent. 
+	 * @see ChatTypes.
+	 */
 	public int getType() {
 		return type;
 	}
@@ -64,14 +77,24 @@ public class ChatEvent {
 		this.source = source;
 	}
 
+	/**
+	 * @param time The time that this chat event occurred.
+	 */
 	public void setTime(long time) {
 		this.time = time;
 	}
 
+	/**
+	 * @param type The type of ChatEvent this is.
+	 * @see ChatTypes.
+	 */
 	public void setType(int type) {
 		this.type = type;
 	}
 
+	/**
+	 * Dumps information about this ChatEvent to a string.
+	 */
 	@Override
 	public String toString() {
 		return "ChatEvent: source=" + source + " type=" + type + " gameId="
