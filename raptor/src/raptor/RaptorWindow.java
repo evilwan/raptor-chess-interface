68f95854bcf389f95fd8f4b17492ce776dc90a8c
carsonday <carsonday>
2009-10-11 08:23:34 +0000

    Further optimizations to the left coolbar layout in RaptorWindow

--- raptor/src/raptor/RaptorWindow.java
+++ raptor/src/raptor/RaptorWindow.java
@@ -540,6 +540,7 @@ public class RaptorWindow extends ApplicationWindow {
 	protected RaptorSashForm[] sashes = new RaptorSashForm[6];
 	protected Composite statusBar;
 	protected Label statusLabel;
+	protected boolean hasLayedOutForMinimizedCoolbar = true;
 
 	protected CoolBar topCoolbar;
 	protected Composite windowComposite;
@@ -648,10 +649,15 @@ public class RaptorWindow extends ApplicationWindow {
 
 		if (!isAFolderMinimized) {
 			leftCoolbar.setVisible(false);
+			if (!!hasLayedOutForMinimizedCoolbar) {
+				windowComposite.layout(true);
+				hasLayedOutForMinimizedCoolbar = true;
+			}
 		} else {
 			leftCoolbar.setVisible(true);
 			leftCoolbar.layout(true);
 			windowComposite.layout(true);
+			hasLayedOutForMinimizedCoolbar = false;
 		}
 	}
 
