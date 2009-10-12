da3bc5e2d2427442b7435a7e5c51a2ca14d2567f
carsonday <carsonday>
2009-10-12 16:30:49 +0000

    Added documentation for the ChatScript code checked in previously.

--- src/raptor/script/ScriptConnectorType.java
+++ src/raptor/script/ScriptConnectorType.java
@@ -1,5 +1,12 @@
 package raptor.script;
 
+/**
+ * An enum for the script connector type. In the future as other connectors are
+ * written they will be added here.
+ */
 public enum ScriptConnectorType {
+	/**
+	 * The ICS script connector type. Both BICS and FICS use this.
+	 */
 	ICS
 }
