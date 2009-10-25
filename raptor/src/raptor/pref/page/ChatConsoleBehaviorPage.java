9463490a0bae18324c31f8d425148ac8021f24f5
carsonday <carsonday>
2009-10-25 13:59:18 +0000

    Fixes Issue 42. Also updated preferences, got rid of the ChatConsole behaviors page since it only had two options and moved them to the main ChatConsole page.

--- raptor/src/raptor/pref/page/ChatConsoleBehaviorPage.java
+++ raptor/src/raptor/pref/page/ChatConsoleBehaviorPage.java
@@ -1,10 +1,8 @@
 package raptor.pref.page;
 
-import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 
 import raptor.Raptor;
-import raptor.pref.PreferenceKeys;
 import raptor.pref.fields.LabelButtonFieldEditor;
 
 public class ChatConsoleBehaviorPage extends FieldEditorPreferencePage {
@@ -18,13 +16,6 @@ public class ChatConsoleBehaviorPage extends FieldEditorPreferencePage {
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new BooleanFieldEditor(
-				PreferenceKeys.CHAT_IS_PLAYING_CHAT_ON_PTELL,
-				"Play 'chat' sound on all partner tells.",
-				getFieldEditorParent()));
-		addField(new BooleanFieldEditor(
-				PreferenceKeys.CHAT_IS_PLAYING_CHAT_ON_PERSON_TELL,
-				"Play 'chat' sound on all person tells.",
-				getFieldEditorParent()));
+
 	}
 }
