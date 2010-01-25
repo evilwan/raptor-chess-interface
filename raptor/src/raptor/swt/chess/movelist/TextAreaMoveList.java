14aafd3a4bf40fe4ba84358fccc54c68c58a0153
bvk256 <bvk256>
2010-01-25 14:54:12 +0000

    additional Text movelist; mostly is done, but generally not finished

---
+++ raptor/src/raptor/swt/chess/movelist/TextAreaMoveList.java
@@ -0,0 +1,202 @@
+/**
+ * New BSD License
+ * http://www.opensource.org/licenses/bsd-license.php
+ * Copyright (c) 2009, RaptorProject (http://code.google.com/p/raptor-chess-interface/)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * Neither the name of the RaptorProject nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package raptor.swt.chess.movelist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+
+import raptor.chess.Game;
+import raptor.chess.pgn.PgnHeader;
+import raptor.chess.util.GameUtils;
+import raptor.swt.chess.ChessBoardController;
+import raptor.swt.chess.ChessBoardMoveList;
+
+/**
+ * Simple movelist that uses SWT StyledText. This would allow to support
+ * annotation and variation trees within the widget.
+ * 
+ * TODO add MouseListener to support choosing a move
+ * TODO allow annotations
+ */
+public class TextAreaMoveList implements ChessBoardMoveList {
+	private static final Log LOG = LogFactory.getLog(SimpleMoveList.class);
+	
+	protected ChessBoardController controller;
+	protected StyledText textPanel;
+	
+	/**
+	 * Defines offsets for each move node, where the list index is the move number
+	 */
+	protected List<Integer> moveNodes;
+	
+	/**
+	 * Defines lengths (in chars) for each move node
+	 */
+	protected List<Integer> moveNodesLengths;
+	
+	/**
+	 * Offset where the header ends
+	 */
+	protected int movesTextStart;
+	protected Color moveSelectionColor;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void clear() {
+		textPanel.replaceTextRange(0, textPanel.getCharCount(), "");
+		moveNodes.clear();
+		moveNodesLengths.clear();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Composite create(Composite parent) {
+		if (textPanel == null) {
+			createControls(parent);
+		}
+		return textPanel;		
+	}
+
+	private void createControls(Composite parent) {		
+		textPanel = new StyledText(parent, SWT.BORDER | SWT.V_SCROLL);
+		textPanel.setEditable(false);
+		textPanel.setWordWrap(true);
+		moveSelectionColor = new Color(Display.getCurrent(), 165, 192, 255);
+		moveNodes = new ArrayList<Integer>();
+		moveNodesLengths = new ArrayList<Integer>();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void forceRedraw() {
+		updateToGame();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ChessBoardController getChessBoardController() {
+		return controller;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Composite getControl() {
+		return textPanel;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void select(int halfMoveIndex) {
+		if (halfMoveIndex >= moveNodes.size()
+				|| halfMoveIndex < 0)
+			return;
+
+		StyleRange sR = new StyleRange();
+	    sR.background = moveSelectionColor;
+	    sR.start = moveNodes.get(halfMoveIndex);
+	    sR.length = moveNodesLengths.get(halfMoveIndex);;
+		textPanel.replaceStyleRanges(movesTextStart, textPanel.getCharCount()
+				- movesTextStart, new StyleRange[] { sR });
+		textPanel.setCaretOffset(sR.start+sR.length);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setController(ChessBoardController controller) {
+		this.controller = controller;		
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void updateToGame() {
+		if (textPanel.isVisible()) {
+			long startTime = System.currentTimeMillis();
+		
+			if (textPanel.getCharCount() == 0) {
+				String sd = textPanel.getLineDelimiter();
+				textPanel.append(controller.getGame()
+						.getHeader(PgnHeader.White) + " vs "
+						+ controller.getGame().getHeader(PgnHeader.Black)
+						+ sd + sd);
+				StyleRange styleRange = new StyleRange();
+				styleRange.start = 0;
+				styleRange.length = controller.getTitle().length();
+				styleRange.fontStyle = SWT.BOLD;
+				textPanel.setStyleRange(styleRange);
+				movesTextStart = textPanel.getCharCount();
+			}
+
+			Game game = controller.getGame();
+			int moveListSize = game.getMoveList().getSize();
+			if (moveListSize == 0) {
+				textPanel.replaceTextRange(movesTextStart, textPanel
+						.getCharCount(), "");
+			} else {
+				if (moveListSize-moveNodes.size() == 1) {
+					appendMove(moveListSize);
+				}
+				else {
+					if (moveListSize==moveNodes.size())
+						return;					
+					
+					StringBuffer buff = new StringBuffer();
+					for (int i = 0; i < moveListSize; i++) {
+						int start, length;
+						start = movesTextStart+buff.length();
+						buff.append((i % 2 == 0) ? Integer.toString((i+3)/2)
+								+ "." : "");
+						buff.append(GameUtils.convertSanToUseUnicode(game
+								.getMoveList().get(i)
+								.toString(), true));
+						length = buff.length()-(start-movesTextStart);
+						moveNodes.add(start);
+						moveNodesLengths.add(length);
+						buff.append(" ");						
+					}
+					textPanel.append(buff.toString());
+					select(moveListSize-1);
+				}
+			}
+			
+			if (LOG.isDebugEnabled()) {
+				LOG.debug("Updated to game in : "
+						+ (System.currentTimeMillis() - startTime));
+			}
+		}
+		
+	}
+
+	private void appendMove(int moveListSize) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
