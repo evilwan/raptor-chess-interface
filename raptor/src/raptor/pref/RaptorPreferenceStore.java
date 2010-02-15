61239cdafce5fec60b2be0053571a570a55384ef
carsonday <carsonday>
2010-02-15 01:50:49 +0000

    Changed version to RC1a

--- raptor/src/raptor/pref/RaptorPreferenceStore.java
+++ raptor/src/raptor/pref/RaptorPreferenceStore.java
@@ -399,7 +399,7 @@ public class RaptorPreferenceStore extends PreferenceStore implements
 		setDefault(ACTION_SEPARATOR_SEQUENCE, 400);
 
 		// App settings.
-		setDefault(APP_NAME, "Raptor .98 RC1");
+		setDefault(APP_NAME, "Raptor .98 RC1a");
 		setDefault(APP_IS_SHOWING_CHESS_PIECE_UNICODE_CHARS, !OSUtils
 				.isLikelyWindowsXP());
 		setDefault(APP_SASH_WIDTH, 8);
