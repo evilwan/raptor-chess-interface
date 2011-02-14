856dd79912365f3a21f79cb7db341ee81fb1131d
bvk256 <bvk256>
2011-02-14 20:08:27 +0000

    Internationalization: ObservingMouseAction and Seek pages labels workup

--- raptor/src/raptor/pref/page/ObservingMouseActionsPage.java
+++ raptor/src/raptor/pref/page/ObservingMouseActionsPage.java
@@ -21,23 +21,26 @@ import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Label;
 
 import raptor.Raptor;
+import raptor.international.L10n;
 import raptor.pref.PreferenceKeys;
 import raptor.swt.chess.controller.ObservingMouseAction;
 
 public class ObservingMouseActionsPage extends FieldEditorPreferencePage {
 
+	protected static L10n local = L10n.getInstance();
+	
 	public static final String[][] OPTIONS = {
-			{ "None", ObservingMouseAction.None.toString() },
-			{ "Make Primary Game (For kib and whisper)",
+			{ local.getString("none"), ObservingMouseAction.None.toString() },
+			{ local.getString("makePrimGame"),
 					ObservingMouseAction.MakePrimaryGame.toString() },
-			{ "Match Winner", ObservingMouseAction.MatchWinner.toString() },
-			{ "Open Game Tell Tab",
+			{ local.getString("matchWinner"), ObservingMouseAction.MatchWinner.toString() },
+			{ local.getString("openGameTTab"),
 					ObservingMouseAction.AddGameChatTab.toString() } };
 
 	public ObservingMouseActionsPage() {
 		// Use the "flat" layout
 		super(GRID);
-		setTitle("Observing");
+		setTitle(local.getString("observing"));
 		setPreferenceStore(Raptor.getInstance().getPreferences());
 	}
 
@@ -45,26 +48,26 @@ public class ObservingMouseActionsPage extends FieldEditorPreferencePage {
 	protected void createFieldEditors() {
 		addField(new ComboFieldEditor(PreferenceKeys.OBSERVING_CONTROLLER
 				+ PreferenceKeys.LEFT_MOUSE_BUTTON_ACTION,
-				"Left Mouse Button Action:", OPTIONS, getFieldEditorParent()));
+				local.getString("lmouseButtAct"), OPTIONS, getFieldEditorParent()));
 
 		addField(new ComboFieldEditor(PreferenceKeys.OBSERVING_CONTROLLER
 				+ PreferenceKeys.RIGHT_MOUSE_BUTTON_ACTION,
-				"Right Mouse Button Action:", OPTIONS, getFieldEditorParent()));
+				local.getString("rmouseButtAct"), OPTIONS, getFieldEditorParent()));
 
 		addField(new ComboFieldEditor(PreferenceKeys.OBSERVING_CONTROLLER
 				+ PreferenceKeys.MIDDLE_MOUSE_BUTTON_ACTION,
-				"Middle Mouse Button Action:", OPTIONS, getFieldEditorParent()));
+				local.getString("mmouseButtAct"), OPTIONS, getFieldEditorParent()));
 
 		addField(new ComboFieldEditor(PreferenceKeys.OBSERVING_CONTROLLER
 				+ PreferenceKeys.MISC1_MOUSE_BUTTON_ACTION,
-				"Misc1 Mouse Button Action:", OPTIONS, getFieldEditorParent()));
+				local.getString("misc1mouseButtAct"), OPTIONS, getFieldEditorParent()));
 
 		addField(new ComboFieldEditor(PreferenceKeys.OBSERVING_CONTROLLER
 				+ PreferenceKeys.MISC2_MOUSE_BUTTON_ACTION,
-				"Misc2 Mouse Button Action:", OPTIONS, getFieldEditorParent()));
+				local.getString("misc2mouseButtAct"), OPTIONS, getFieldEditorParent()));
 
 		final Label label = new Label(getFieldEditorParent(), SWT.LEFT);
-		label.setText("\n      Click here to determine mouse button.      \n ");
+		label.setText(local.getString("clkToDetMButton"));
 		label.setBackground(getShell().getDisplay().getSystemColor(
 				SWT.COLOR_BLUE));
 		label.setForeground(getShell().getDisplay().getSystemColor(
@@ -73,21 +76,21 @@ public class ObservingMouseActionsPage extends FieldEditorPreferencePage {
 
 			public void mouseDoubleClick(MouseEvent e) {
 				if (e.button == 1) {
-					label.setText("Left Double Click");
+					label.setText(local.getString("ldClick"));
 				}
 			}
 
 			public void mouseDown(MouseEvent e) {
 				if (e.button == 1) {
-					label.setText("Left Click");
+					label.setText(local.getString("lClick"));
 				} else if (e.button == 2) {
-					label.setText("Middle Click");
+					label.setText(local.getString("mClick"));
 				} else if (e.button == 3) {
-					label.setText("Right Click");
+					label.setText(local.getString("rClick"));
 				} else if (e.button == 4) {
-					label.setText("Misc1 Click");
+					label.setText(local.getString("misc1Click"));
 				} else if (e.button == 5) {
-					label.setText("Misc2 Click");
+					label.setText(local.getString("misc2Click"));
 				}
 			}
 
--- raptor/src/raptor/pref/page/SeekPage.java
+++ raptor/src/raptor/pref/page/SeekPage.java
@@ -5,39 +5,42 @@ import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 
 import raptor.Raptor;
+import raptor.international.L10n;
 import raptor.pref.PreferenceKeys;
 import raptor.service.SeekService.SeekType;
 
 public class SeekPage extends FieldEditorPreferencePage {
+	
+	protected static L10n local = L10n.getInstance();
 
 	public static final String[][] SEEK_OUTPUT_TYPES = {
-			{ "Show all seeks", SeekType.AllSeeks.toString() },
-			{ "Show formula filtered seeks",
+			{ local.getString("showAllSeeks"), SeekType.AllSeeks.toString() },
+			{ local.getString("showFormula"),
 					SeekType.FormulaFiltered.toString() } };
 
 	public SeekPage() {
 		// Use the "flat" layout
 		super(GRID);
-		setTitle("Seeks");
+		setTitle(local.getString("seeks"));
 		setPreferenceStore(Raptor.getInstance().getPreferences());
 	}
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new ComboFieldEditor(PreferenceKeys.SEEK_OUTPUT_TYPE, "Seek output type:",
+		addField(new ComboFieldEditor(PreferenceKeys.SEEK_OUTPUT_TYPE, local.getString("seekOutType"),
 				SEEK_OUTPUT_TYPES, getFieldEditorParent()));
 
 		addField(new ColorFieldEditor(PreferenceKeys.SEEK_GRAPH_COMPUTER_COLOR,
-				"Seek Graph Computer Color:", getFieldEditorParent()));
+				local.getString("seekGraphCompCol"), getFieldEditorParent()));
 
 		addField(new ColorFieldEditor(PreferenceKeys.SEEK_GRAPH_MANY_COLOR,
-				"Seek Graph Many Color:", getFieldEditorParent()));
+				local.getString("seekGraphManyCol"), getFieldEditorParent()));
 
 		addField(new ColorFieldEditor(PreferenceKeys.SEEK_GRAPH_RATED_COLOR,
-				"Seek Graph Rated Color:", getFieldEditorParent()));
+				local.getString("seekGraphRatCol"), getFieldEditorParent()));
 
 		addField(new ColorFieldEditor(PreferenceKeys.SEEK_GRAPH_UNRATED_COLOR,
-				"Seek Graph Unrated Color:", getFieldEditorParent()));
+				local.getString("seekGraphUnratCol"), getFieldEditorParent()));
 	}
 
 }
