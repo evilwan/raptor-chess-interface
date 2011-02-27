be65d54ec55d8c27f0f28581629373ac02eee59b
carsonday <carsonday>
2011-02-27 21:50:09 +0000

    Removed imports not being used.

--- raptor/src/raptor/swt/InputDialog.java
+++ raptor/src/raptor/swt/InputDialog.java
@@ -16,8 +16,6 @@ package raptor.swt;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Point;
-import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
