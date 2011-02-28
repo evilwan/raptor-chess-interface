36192bcd5ce36426215dfa471f3cfb856ccf8a80
carsonday <carsonday>
2011-02-28 03:12:51 +0000

    Added a redraw in playing controller after a premove drop is made.

--- raptor/src/raptor/swt/chess/controller/PlayingController.java
+++ raptor/src/raptor/swt/chess/controller/PlayingController.java
@@ -1124,6 +1124,7 @@ public class PlayingController extends ChessBoardController {
 			}
 		}
 		adjustPremoveLabelHighlightsAndArrows();
+		board.redrawPiecesAndArtifacts(false);
 		return result;
 	}
 
