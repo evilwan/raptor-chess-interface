4dc69c5d9b01b3a80bb854fa35d8357285ef5102
carsonday <carsonday>
2011-04-03 16:35:25 +0000

    Removed an extraneous import.

--- raptor/src/raptor/pref/RaptorPreferenceStore.java
+++ raptor/src/raptor/pref/RaptorPreferenceStore.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
