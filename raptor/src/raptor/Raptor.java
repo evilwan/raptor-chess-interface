14b501b69f188e4c289a71dc98839d03c33f4544
carsonday <carsonday>
2011-06-28 00:16:12 +0000

    Fixed a bug where saved games were being overwritten each time Raptor started. StrategyMaster helped me track this one down. raptorGames was in defaultHomeDirectory/games. When raptor starts it called Rpator.install() which copies over files in those directories, so the games file was being lost each time. Now raptor tries to create the APP_PGN_FILE stored in the preferences only if it does not exist.

--- raptor/src/raptor/Raptor.java
+++ raptor/src/raptor/Raptor.java
@@ -650,6 +650,10 @@ public class Raptor implements PreferenceKeys {
 	private void install() {
 		try {
 			FileUtils.copyFiles(DEFAULT_HOME_DIR, USER_RAPTOR_DIR);
+			
+			if (!new File(getPreferences().getString(APP_PGN_FILE)).exists()) {
+				new File(getPreferences().getString(APP_PGN_FILE)).createNewFile();
+			}
 		} catch (IOException ioe) {
 			throw new RuntimeException(ioe);
 		}
