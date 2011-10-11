3045b6a036899210d4d75b696999206ddaecb7c3
bvk256 <bvk256>
2011-10-11 14:03:13 +0000

    Fixed Issue 625

--- raptor/src/raptor/engine/uci/UCIMove.java
+++ raptor/src/raptor/engine/uci/UCIMove.java
@@ -41,8 +41,9 @@ public class UCIMove {
 		startSquare = GameUtils.getSquare(uciString.substring(0, 2));
 		endSquare = GameUtils.getSquare(uciString.substring(2, 4));
 		if (uciString.length() > 4) {
-			char pieceChar = uciString.charAt(4);
-			promotedPiece = GameConstants.PIECE_TO_SAN.indexOf(pieceChar);
+			char pieceChar = Character.toLowerCase(uciString.charAt(4));
+			promotedPiece = GameConstants.PIECE_TO_SAN
+					.toLowerCase().indexOf(pieceChar);
 		}
 	}
 
