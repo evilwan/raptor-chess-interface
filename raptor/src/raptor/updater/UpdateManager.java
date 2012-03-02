edf29ac8e249f1d51de6e9353fa9bc809ba19dd1
bvk256 <bvk256>
2012-03-02 19:59:15 +0000

    A couple of enhancements to the update manager

--- raptor/src/raptor/updater/UpdateManager.java
+++ raptor/src/raptor/updater/UpdateManager.java
@@ -30,14 +30,14 @@ public class UpdateManager {
 			System.getProperty("user.home") + "/" + APP_HOME_DIR
 					+ "/raptor.properties");
 
-	private boolean isUpdateOn;
-	private boolean isReadyToUpdate;
+	private boolean isUpdateOn = false;
+	private boolean isReadyToUpdate = false;
 	private static String appVersionStr = ".98u3";
-	
+
 	protected static boolean isLikelyLinux = false;
 	protected static boolean isLikelyOSX = false;
 	protected static boolean isLikelyWindows = false;
-	
+
 	static {
 		String osName = System.getProperty("os.name");
 		if (osName.startsWith("Mac OS")) {
@@ -48,24 +48,24 @@ public class UpdateManager {
 			isLikelyLinux = true;
 		}
 	}
-	
+
 	private Label infoLabel1;
 	private Label infoLabel2;
-	
-	private Thread upgradeThread;
+
+	Thread upgradeThread;
 	private Shell shell;
 
 	public static void center(Shell shell) {
-        Rectangle bds = shell.getDisplay().getBounds();
-        Point p = shell.getSize();
+		Rectangle bds = shell.getDisplay().getBounds();
+		Point p = shell.getSize();
 
-        int nLeft = (bds.width - p.x) / 2;
-        int nTop = (bds.height - p.y) / 2;
+		int nLeft = (bds.width - p.x) / 2;
+		int nTop = (bds.height - p.y) / 2;
 
-        shell.setBounds(nLeft, nTop, p.x, p.y);
+		shell.setBounds(nLeft, nTop, p.x, p.y);
 	}
-	
-	public void invokeMain(String args[]) {
+
+	public static void invokeMain(String args[]) {
 
 		File f = new File("Raptor.jar");
 
@@ -80,103 +80,110 @@ public class UpdateManager {
 			ex.printStackTrace();
 		}
 	}
-	
-	public void open() {
-        Display display = new Display();
-        shell = new Shell(display, SWT.DIALOG_TRIM | SWT.MIN);        
-        shell.setText("Raptor upgrade");
-        shell.addListener(SWT.Close, new Listener() {
 
-            public void handleEvent(Event event) {
-                return;
-            }
-        });
-        createGuiContents(shell);
-        shell.pack();
-        center(shell);
-        shell.open();
-        while (!shell.isDisposed()) {
-            if (!display.readAndDispatch()) {
-                display.sleep();
-            }
-        }
-        display.close();
-    }
-	
+	public void open() {
+		Display display = new Display();
+		shell = new Shell(display, SWT.DIALOG_TRIM | SWT.MIN);
+		shell.setText("Raptor upgrade");
+		shell.addListener(SWT.Close, new Listener() {
+
+			public void handleEvent(Event event) {
+				upgradeThread.interrupt();
+			}
+		});
+		createGuiContents(shell);
+		shell.pack();
+		center(shell);
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+		display.close();
+	}
+
 	void createGuiContents(final Shell shell) {
 		GridLayout gridLayout = new GridLayout();
-        gridLayout.numColumns = 1;        
-        gridLayout.marginWidth = 9;
-        shell.setLayout(gridLayout);
+		gridLayout.numColumns = 1;
+		gridLayout.marginWidth = 9;
+		shell.setLayout(gridLayout);
 
-        Label label = new Label(shell, SWT.NULL);
-        label.setText("Update initiated...               ");
-        
-        infoLabel1 = new Label(shell, SWT.NULL);
-        infoLabel1.setText("                                                                                        ");
-        infoLabel2 = new Label(shell, SWT.NULL);
-        infoLabel2.setText("                                                                                        ");
-        
-        Button bt = new Button(shell, SWT.PUSH);
-        bt.setText("Cancel");
-        bt.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
-                false, 1, 1));
-        bt.addListener(SWT.Selection, new Listener() {
+		Label label = new Label(shell, SWT.NULL);
+		label.setText("Update initiated...               ");
 
-            public void handleEvent(Event event) {   
-            	upgradeThread.interrupt();
-                shell.close();
-            }
-        });
-	}
-	
-	public void upgrade() {
-		Display.getDefault().syncExec(new Runnable() {
+		infoLabel1 = new Label(shell, SWT.NULL);
+		infoLabel1
+				.setText("                                                                                        ");
+		infoLabel2 = new Label(shell, SWT.NULL);
+		infoLabel2
+				.setText("                                                                                        ");
 
-			@Override
-			public void run() {
-				infoLabel1.setText("Initializing the remote connection...");							
+		Button bt = new Button(shell, SWT.PUSH);
+		bt.setText("Cancel");
+		bt.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		bt.addListener(SWT.Selection, new Listener() {
+
+			public void handleEvent(Event event) {
+				upgradeThread.interrupt();
+				shell.close();
 			}
-			
 		});
+	}
+
+	public void upgrade() {
+		if (!infoLabel1.isDisposed()) {
+			Display.getDefault().syncExec(new Runnable() {
+
+				@Override
+				public void run() {
+					infoLabel1.setText("Initializing the remote connection...");
+				}
+
+			});
+		}
 		try {
 			boolean forVersionSet = false;
 			URL google = new File("test").toURI().toURL();
 			BufferedReader bin = new BufferedReader(new InputStreamReader(
-					google.openStream()),1024);
-			String currentLine = null;
+					google.openStream()), 1024);
+			String currentLine;
 			while ((currentLine = bin.readLine()) != null) {
 				if (currentLine.startsWith("for-version:") && forVersionSet)
 					break;
 				else if (currentLine.startsWith("for-version:")
 						&& currentLine.substring(13).equals(appVersionStr)) {
 					forVersionSet = true;
-				}
-				else if (forVersionSet && currentLine.startsWith("file:")) {
-					Display.getDefault().asyncExec(new Runnable() {
+				} else if (forVersionSet && currentLine.startsWith("file:")) {
+					if (!infoLabel2.isDisposed()) {
+						Display.getDefault().asyncExec(new Runnable() {
 
-						@Override
-						public void run() {
-							infoLabel2.setText("Downloading upgrade files...");							
-						}
-						
-					});
-					
+							@Override
+							public void run() {
+								infoLabel2
+										.setText("Downloading upgrade files...");
+							}
+
+						});
+					}
+
 					String data[] = currentLine.substring(6).split(" ");
-					String tempFilename = System.getProperty("java.io.tmpdir")+
-							System.getProperty("file.separator") + Math.abs(data[1].hashCode());
+					String tempFilename = System.getProperty("java.io.tmpdir")
+							+ System.getProperty("file.separator")
+							+ Math.abs(data[1].hashCode());
 					URL fileUrl = new URL(data[0]);
-				    ReadableByteChannel rbc2 = Channels.newChannel(fileUrl.openStream());
-				    FileOutputStream fos2 = new FileOutputStream(tempFilename);
-				    fos2.getChannel().transferFrom(rbc2, 0, 1 << 24);
-				    fos2.close();				
-				    if (isLikelyOSX)
-				    	applyOSX(tempFilename, data[1]);
-				    else if (isLikelyWindows)
-				    	applyWindows(tempFilename, data[1]);
-				    else if (isLikelyLinux)
-				    	applyLinux(tempFilename, data[1]);
-				    	
+					ReadableByteChannel rbc2 = Channels.newChannel(fileUrl
+							.openStream());
+					FileOutputStream fos2 = new FileOutputStream(tempFilename);
+					fos2.getChannel().transferFrom(rbc2, 0, 1 << 24);
+					fos2.close();
+					if (isLikelyOSX)
+						applyOSX(tempFilename, data[1]);
+					else if (isLikelyWindows)
+						applyWindows(tempFilename, data[1]);
+					else if (isLikelyLinux)
+						applyLinux(tempFilename, data[1]);
+
 					System.out.println(data[1]);
 				}
 			}
@@ -185,59 +192,63 @@ public class UpdateManager {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-		Display.getDefault().syncExec(new Runnable() {
+		if (!shell.isDisposed()) {
+			Display.getDefault().syncExec(new Runnable() {
 
-			@Override
-			public void run() {
-				shell.close();							
-			}
-			
-		});
+				@Override
+				public void run() {
+					shell.close();
+				}
+
+			});
+		}
 	}
 
-	private void applyLinux(String tempFilename, String dest) {
+	private static void applyLinux(String tempFilename, String dest) {
 		Process p;
 		try {
-			p = Runtime.getRuntime().exec("gksudo mv "+tempFilename+" "+dest);
-			if (p.waitFor()!=0) {
-				p = Runtime.getRuntime().exec("kdesudo mv "+tempFilename+" "+dest);
+			p = Runtime.getRuntime().exec(
+					"gksudo mv " + tempFilename + " " + dest);
+			if (p.waitFor() != 0) {
+				p = Runtime.getRuntime().exec(
+						"kdesudo mv " + tempFilename + " " + dest);
 				p.waitFor();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
-		
+		}
+
 	}
 
-	private void applyWindows(String tempFilename, String dest) {
+	private static void applyWindows(String tempFilename, String dest) {
 		String[] command = new String[3];
 		command[0] = "cmd";
 		command[1] = "/c";
-		command[2] = "copy "+tempFilename+" "+dest;
+		command[2] = "copy " + tempFilename + " " + dest;
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
-			p.waitFor(); 
+			p.waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 	
+		}
 	}
 
-	private void applyOSX(String tempFilename, String dest) {
+	private static void applyOSX(String tempFilename, String dest) {
 		Process p;
 		try {
-			p = Runtime.getRuntime().exec("mv "+tempFilename+" "+dest);
-			p.waitFor(); 
+			p = Runtime.getRuntime().exec("mv " + tempFilename + " " + dest);
+			p.waitFor();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 		
+		}
 	}
 
 	void checkPrefs() {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(
 					USER_RAPTOR_PREF));
-			String currentLine = null;
+			String currentLine;
 			while ((currentLine = reader.readLine()) != null) {
 				if (currentLine.startsWith("app-update")) {
 					isUpdateOn = Boolean
@@ -246,13 +257,12 @@ public class UpdateManager {
 						reader.close();
 						return;
 					}
-				}
-				else if (currentLine.startsWith("app-version")) {
+				} else if (currentLine.startsWith("app-version")) {
 					appVersionStr = currentLine.substring(12);
+				} else if (currentLine.startsWith("ready-to-update")) {
+					isReadyToUpdate = Boolean.parseBoolean(currentLine
+							.substring(16));
 				}
-				else if (currentLine.startsWith("ready-to-update")) {
-					isReadyToUpdate = Boolean.parseBoolean(currentLine.substring(16));
-				}				
 			}
 			reader.close();
 		} catch (IOException e) {
@@ -265,29 +275,32 @@ public class UpdateManager {
 	 */
 	public static void main(String[] args) {
 		final UpdateManager manager = new UpdateManager();
-		
+
 		manager.checkPrefs();
 		if (manager.isUpdateOn && manager.isReadyToUpdate) {
+			manager.upgradeThread = Thread.currentThread();
 			Thread th = new Thread(new Runnable() {
 
 				@Override
 				public void run() {
 					manager.open();
 				}
-				
+
 			});
 			th.start();
-			
+
+			boolean threadDisposed = false;
 			try {
 				Thread.sleep(600);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				threadDisposed = true;
 			}
-			manager.upgrade();						
+			
+			if (!threadDisposed)
+				manager.upgrade();
 		}
 
-		manager.invokeMain(args);
+		invokeMain(args);
 	}
 
 }
